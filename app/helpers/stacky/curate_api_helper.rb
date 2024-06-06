4a811dc10afa86015267d68c0b4e6b9237f5307e
klxu03 <beaver2025@gmail.com>
2024-06-06 19:26:50 +0000

    Mastodon server sending post request to curate server

--- app/helpers/stacky/curate_api_helper.rb
+++ app/helpers/stacky/curate_api_helper.rb
@@ -17,11 +17,11 @@ module Stacky::CurateApiHelper
     puts "Error: #{e.message}"
     nil
   end
-  
+
   def self.post(url, body)
     uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri)
+    request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'application/json'})
     request.body = body.to_json
     response = http.request(request)
 
@@ -41,16 +41,16 @@ module Stacky::CurateApiHelper
     # and then call the GET api endpoints
     # self.get("http://example.com/api/endpoint")
     # or POST
-    # body = { text: status.text, id: status.id }
-    # self.post("http://example.com/api/update_index", body)
-    "stub index success >>>> status: #{status.id} msg: #{status.text}"
+    body = { body: status.text, id: status.id }
+    post("http://beta.stacky.social:3002/insert", body)
+    "stub index success >>>> status id: #{status.id} msg: #{status.text}"
   end
   def self.update_index_status(status)
     # extract the status information
     # TODO
     # and then call the post to api endpoints
-    # body = { text: status.text, id: status.id }
-    # self.post("http://example.com/api/update_index", body)
+    body = { body: status.text, id: status.id }
+    post("http://beta.stacky.social:3002/update", body)
     "stub update index success >>>> status: #{status.id} msg: #{status.text}"
   end
 
